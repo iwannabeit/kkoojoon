b6c1a08d333782cc25f9bc8b82d4026ba4d080dd
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2025-11-19 23:07:09 +0900

    first commit

--- src/main/java/org/programmers/HighScoreKit/StackQ_02.java
+++ src/main/java/org/programmers/HighScoreKit/StackQ_02.java
@@ -3,5 +3,6 @@ package org.programmers.HighScoreKit;
 public class StackQ_02 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
