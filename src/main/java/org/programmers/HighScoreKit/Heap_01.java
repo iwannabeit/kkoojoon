3eeb936e8ff6cd2cf7e951f27a8d69a4f2901e48
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2025-12-08 22:53:56 +0900

    힙 구현(삽입/삭제) 완.

--- src/main/java/org/programmers/HighScoreKit/Heap_01.java
+++ src/main/java/org/programmers/HighScoreKit/Heap_01.java
@@ -4,15 +4,16 @@ import java.util.Arrays;
 
 public class Heap_01 {
     public static void main(String[] args){
-        Minheap minheap = new Minheap();
+        Maxheap maxheap = new Maxheap();
 
-        int[] result = minheap.insert(8, new int[]{9,7,6,5,4,3,2,2,1,3});
+//        int[] result = maxheap.insert(8, new int[]{9,7,6,5,4,3,2,2,1,3});
+        int[] result = maxheap.delete(new int[]{9,7,6,5,4,3,2,2,1,3});
         System.out.println(Arrays.toString(result));
     }
 
 }
 
-class Minheap{
+class Maxheap{
 
     public int[] insert(int value, int[] arry){
         //heap 새로 생성
@@ -43,18 +44,25 @@ class Minheap{
             heap[i+1] = arry[i];
         }
         
-        //루트노드 삭제(최대값 삭제)
-        heap[heap.length-1] = 0;
+        // 마지막 자식을 루트로(최대값 삭제)
         heap[1] = heap[heap.length-1];
+        heap[heap.length-1] = 0;
 
         
-        for(int i=1; i<heap.length; i++){
-            if(heap[i] < heap[2*i]){
+        for(int i=1; 2*i<heap.length; i++){
+            if(heap[i] > heap[2*i] && heap[i] > heap[2*i+1]){
+                break;
+            }
+            //왼쪽 자식 노드 보다 작으면 바꾸기
+            else if(heap[i] < heap[2*i]){
                 int tmp = heap[i];
                 heap[i] = heap[2*i];
+                heap[2*i] = tmp;
             }
             else if( heap[i] < heap[2*i+1]){
-
+                int tmp = heap[i];
+                heap[i] = heap[2*i+1];
+                heap[2*i+1] = tmp;
             }
         }
 
