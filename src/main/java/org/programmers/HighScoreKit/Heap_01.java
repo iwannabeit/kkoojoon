bdc626d7fd0a49b04713f3a5b3e7c1d8112f722e
iwannabeit <skzx737@naver.com>
2025-12-10 17:59:34 +0900

    시간효율성까지 통과. 더맵게 문풀 [완료]

--- src/main/java/org/programmers/HighScoreKit/Heap_01.java
+++ src/main/java/org/programmers/HighScoreKit/Heap_01.java
@@ -4,140 +4,101 @@ import java.util.Arrays;
 
 public class Heap_01 {
     public static void main(String[] args){
-        Maxheap maxheap = new Maxheap();
+        // Maxheap maxheap = new Maxheap();
 
 //        int[] result = maxheap.insert(8, new int[]{9,7,6,5,4,3,2,2,1,3});
         // int[] result = maxheap.delete(new int[]{9,7,6,5,4,3,2,2,1,3});
         // System.out.println(Arrays.toString(result));
-        int[] answer = solution(new int[]{1, 2, 3,9,10,12}, 50);
-        // System.out.println(answer);
+        int answer = solution(new int[]{1, 1, 1,1,1,1}, 7);
+        System.out.println(answer);
     }
 
-    public static int[] solution(int[] scoville, int K){
+    public static int solution(int[] scoville, int K){
         Minheap minheap = new Minheap();
-        //heap으로 만들기 위한 배열 복사
+
         int[] heap = new int[scoville.length+1];
-        heap[0] = 0;
+        int heapsize = 0;
         
-        // for(int i=0; i<scoville.length; i++){
-        //     heap[i+1] = scoville[i];
-        // }
 
         for(int j=0; j<scoville.length; j++){
-            minheap.insert(scoville[j], heap);
+            heapsize = minheap.insert(scoville[j], heap, heapsize);
         }
 
-        return heap;
+        int trial = 0;
+
+        while(heap[1] < K && heapsize >= 2){
+
+            int min1 = heap[1];
+            heapsize = minheap.delete(heap, heapsize);
+            int min2 = heap[1];
+            heapsize = minheap.delete(heap, heapsize);
+
+            int mix = min1+(min2*2);
+            heapsize = minheap.insert(mix, heap, heapsize);
+            trial++;
+        }
+        if(heap[1] < K){
+            return -1;
+        }
+        return trial;
+
+        
     }
 
 }
 
 class Minheap{
-    public int[] insert(int value, int[] heap){
-
-        if(heap[1] == 1){
-            heap[1] = value;
-            return heap;
-        }
-        int idx = heap.length-1;
+    public int insert(int value, int[] heap, int heapsize){
+        heapsize++;
+        int idx = heapsize;
 
         while(idx != 1 && value < heap[idx/2]){
             heap[idx] = heap[idx/2];
-
             idx = idx/2;
         }
         heap[idx] = value;
-        return heap;
+        return heapsize;
     }
 
-    public int[] delete(int[] arry){
-        //heap 새로 생성
-        int[] heap = new int[arry.length+1];
-        heap[0] = 0;
-        //복사
-        for(int i=0; i<arry.length; i++){
-            heap[i+1] = arry[i];
+    public int delete(int[] heap, int heapsize){
+        if(heapsize == 0){
+            return 0;
         }
 
-        // 마지막 자식을 루트로(최대값 삭제)
-        heap[1] = heap[heap.length-1];
-        heap[heap.length-1] = 0;
+        // 마지막 자식을 루트로(최소값 삭제)
+        heap[1] = heap[heapsize];
+        heap[heapsize] = -1;
+        heapsize--;
 
+        int i=1;
 
-        for(int i=1; 2*i<heap.length; i++){
-            if(heap[i] < heap[2*i] && heap[i] < heap[2*i+1]){
+        while(true){
+            int left = 2*i;
+            int right = 2*i+1;
+
+            // 자식이 없다 : 종료
+            if(left > heapsize){
                 break;
             }
-            //왼쪽 자식 노드 보다 크면 바꾸기
-            else if(heap[i] > heap[2*i]){
-                int tmp = heap[i];
-                heap[i] = heap[2*i];
-                heap[2*i] = tmp;
-            }
-            else if( heap[i] > heap[2*i+1]){
-                int tmp = heap[i];
-                heap[i] = heap[2*i+1];
-                heap[2*i+1] = tmp;
-            }
-        }
 
-        return heap;
+            //왼/오 중 작은자식
+            int smaller = left;
+            if(right <= heapsize && heap[right] < heap[left]){
+                smaller = right;
+            }
+
+            //부모가 더 작으면 종료
+            if(heap[i] <= heap[smaller]) break;
+
+            //그 외는 swap
+            int tmp = heap[i];
+            heap[i] = heap[smaller];
+            heap[smaller] = tmp;
+
+            //스왑된 자식 위치로
+            i = smaller;
+        }
+        
+        return heapsize;
     }
 }
-
-class Maxheap{
-
-    public int[] insert(int value, int[] arry){
-        //heap 새로 생성
-        int[] heap = new int[arry.length+2];
-        heap[0] = 0;
-        //복사
-        for(int i=0; i<arry.length; i++){
-            heap[i+1] = arry[i];
-        }
-        
-        int idx = heap.length-1;
-
-        while(idx != 1 && value > heap[idx/2]){
-            heap[idx] = heap[idx/2];
-
-            idx = idx/2;
-        }
-        heap[idx] = value;
-        return heap; 
-    }
-
-    public int[] delete(int[] arry){
-        //heap 새로 생성
-        int[] heap = new int[arry.length+1];
-        heap[0] = 0;
-        //복사
-        for(int i=0; i<arry.length; i++){
-            heap[i+1] = arry[i];
-        }
-        
-        // 마지막 자식을 루트로(최대값 삭제)
-        heap[1] = heap[heap.length-1];
-        heap[heap.length-1] = 0;
-
-        
-        for(int i=1; 2*i<heap.length; i++){
-            if(heap[i] > heap[2*i] && heap[i] > heap[2*i+1]){
-                break;
-            }
-            //왼쪽 자식 노드 보다 작으면 바꾸기
-            else if(heap[i] < heap[2*i]){
-                int tmp = heap[i];
-                heap[i] = heap[2*i];
-                heap[2*i] = tmp;
-            }
-            else if( heap[i] < heap[2*i+1]){
-                int tmp = heap[i];
-                heap[i] = heap[2*i+1];
-                heap[2*i+1] = tmp;
-            }
-        }
-
-        return heap;
-    }
- }
