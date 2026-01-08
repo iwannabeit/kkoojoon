4d124b43554d95841383acca3381203b69a6a0c0
iwannabeit <skzx737@naver.com>
2026-01-08 11:10:43 +0900

    좌표 bfs 구현 완

--- src/main/java/org/programmers/HighScoreKit/BfsDfs_03.java
+++ src/main/java/org/programmers/HighScoreKit/BfsDfs_03.java
@@ -13,16 +13,37 @@ public class BfsDfs_03 {
         boolean[][] visited = new boolean[maps.length][maps.length];
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        bfs(dx, dy, visited);
+        bfs(dx, dy, visited, maps);
         
 
         return answer;
     }
 
-    static void bfs(int[] dx, int[] dy, boolean[][] visited){
+    static void bfs(int[] dx, int[] dy, boolean[][] visited, int[][] maps){
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{0, 0});
         visited[0][0] = true;
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for(int i=0; i<4; i++){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+
+                // 범위 체크
+                if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps.length)
+                    continue;
+                //방문 check , map 조건 check 
+                if(!visited[nx][ny] && maps[nx][ny] == 0){
+                    visited[nx][ny] = true;
+                    q.offer(new int[]{nx, ny});
+                }
+
+            }
+        }
         
     }
 }
