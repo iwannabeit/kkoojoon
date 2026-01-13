4aaff1ae02aef41c58cb3f825d0ecf74de28224f
iwannabeit <skzx737@naver.com>
2026-01-13 10:03:25 +0900

    BFS/DFS_03: 문제 풀이 완료(정답)

--- src/main/java/org/programmers/HighScoreKit/BfsDfs_03.java
+++ src/main/java/org/programmers/HighScoreKit/BfsDfs_03.java
@@ -4,14 +4,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BfsDfs_03 {
-    static int cnt = 1;
     public static void main(String[] args){
         
         solution(new int[][]{ {1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}});
     }
 
     static int solution(int[][] maps){
-        boolean[][] visited = new boolean[maps.length][maps.length];
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
         int answer = bfs(dx, dy, visited, maps);
@@ -21,8 +20,10 @@ public class BfsDfs_03 {
 
     static int bfs(int[] dx, int[] dy, boolean[][] visited, int[][] maps){
         Queue<int[]> q = new LinkedList<>();
+        int[][] dist = new int[maps.length][maps[0].length];
         q.offer(new int[]{0, 0});
         visited[0][0] = true;
+        dist[0][0] = 1;
 
         while(!q.isEmpty()){
             int[] cur = q.poll();
@@ -34,21 +35,22 @@ public class BfsDfs_03 {
                 int ny = y+dy[i];
 
                 // 범위 체크
-                if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps.length)
+                if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length)
                     continue;
                 //방문 check , map 조건 check 
                 if(!visited[nx][ny] && maps[nx][ny] == 1){
                     visited[nx][ny] = true;
                     q.offer(new int[]{nx, ny});
-                    cnt += 1;
-                    if(visited[maps.length][maps.length]){
-                        return cnt;
+                    dist[nx][ny] = dist[x][y] + 1;
+                    
+                    if(visited[maps.length-1][maps[0].length-1]){
+                        return dist[nx][ny];
                     }
                 }
 
             }
         }
-        if(!visited[maps.length][maps.length]){
+        if(!visited[maps.length-1][maps[0].length-1]){
             return -1;
         }
         return -1;
