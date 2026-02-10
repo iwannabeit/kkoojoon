608f74be28edc9859866c5046ca6a1af9c551642
iwannabeit <skzx737@naver.com>
2026-02-10 16:08:01 +0900

    .

--- src/main/java/org/baekjoon/DFS/_1012.java
+++ src/main/java/org/baekjoon/DFS/_1012.java
@@ -8,9 +8,13 @@ public class _1012 {
     static int[][] map; 
     static int[] dx = {0,0,-1,1};
     static int[] dy = {-1,1,0,0};
+    static int cnt = 0;
 
     public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        InputStream is = _1012.class.getResourceAsStream("/org/input.txt");
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(is));
         StringTokenizer line1 = new StringTokenizer(bf.readLine());
         
         int T = Integer.parseInt(line1.nextToken());
@@ -22,20 +26,48 @@ public class _1012 {
             int K = Integer.parseInt(mnk.nextToken());
 
             map = new int[N][M];
+            visited = new boolean[N][M];
 
             for(int j=0; j<K; j++){
                 StringTokenizer cabXY = new StringTokenizer(bf.readLine());
                 int cabX = Integer.parseInt(cabXY.nextToken());
                 int cabY = Integer.parseInt(cabXY.nextToken());
-                map[cabX][cabY] = 1;
+                map[cabY][cabX] = 1;
             }
 
+            System.out.println(Arrays.deepToString(map));
+            int x=0, y=0; 
             
+            for(int k=0; k<4; k++){
+                int nx = x+dx[k];
+                int ny = y+dy[k];
+                System.out.print(nx+" "+ny+" ");
+
+                if(nx<0 || ny < 0 || nx > map[1].length || ny > map[0].length){continue;}
+
+                if(!visited[nx][ny] && map[nx][ny] == 1){
+                    dfs(nx, ny);
+                    cnt++;
+                }
+            }
+            System.out.println(cnt);
         }
         
     }
 
-    static void dfs(){
+    static void dfs(int x, int y){
+        visited[x][y] = true;
+        for(int i=0; i<4; i++){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+
+                if(nx<0 || ny < 0 || nx > map[1].length || ny > map[0].length){continue;}
+
+                if(!visited[nx][ny] && map[nx][ny] == 1){
+                    System.out.print(nx+" "+ny+" ");
+                    dfs(nx, ny);
+                }
+            }
         
     }
 }
