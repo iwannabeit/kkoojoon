b34e5ad357bc522aee342cc32ed88ab2bdc4af8f
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2026-06-23 00:17:36 +0900

    .

---
+++ src/main/java/org/programmers/HighScoreKit/BinarySearch_01.java
@@ -0,0 +1,37 @@
+package org.programmers.HighScoreKit;
+
+public class BinarySearch_01 {
+  public static void main(String[] args){
+    System.out.print(solution(6, new int[]{7, 10}));
+  }
+
+  public static long solution(int n, int[] times) {
+      long start = 0;
+      long maxtime = 0;
+      long answer = 0;
+      
+      for(int i: times){
+        maxtime = Math.max(maxtime, i);
+      }
+      long end = maxtime*n;
+
+
+      while(start<=end){
+        long possible = 0;
+        long mid = (start+end)/2;
+
+        for(int i: times){
+          possible += mid/i;
+        }
+
+        if(possible >= n){
+          answer = mid;
+          end = mid-1;
+        }else{
+          start = mid+1;
+        }
+
+      }
+      return answer;
+  }
+}
---
+++ src/main/java/org/programmers/HighScoreKit/Graph_01.java
@@ -0,0 +1,15 @@
+package org.programmers.HighScoreKit;
+
+import java.util.*;
+
+public class Graph_01 {
+  public static void main(String[] args){
+    
+  }
+
+  public static void bfs(int n, int[][] edge){
+    Queue q = new LinkedList<>();
+    
+  }
+  
+}
