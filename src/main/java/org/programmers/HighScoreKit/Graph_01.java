884d68a2dd2817661563566538fda2cdc6c114d8
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2026-06-24 01:04:46 +0900

    Graph_01[가장 먼 노드] : 문제풀이 완료(정답)

--- src/main/java/org/programmers/HighScoreKit/Graph_01.java
+++ src/main/java/org/programmers/HighScoreKit/Graph_01.java
@@ -9,6 +9,8 @@ public class Graph_01 {
 
   static int solution(int n, int[][] edges){
     int[] dist = new int[n+1];
+    int maxDist = 0;
+    int cnt = 0;
     boolean[] visit = new boolean[n+1];
     ArrayList<Integer>[] list = new ArrayList[n+1];
 
@@ -21,26 +23,40 @@ public class Graph_01 {
       list[edge[1]].add(edge[0]);
     }
 
-    bfs(list, dist, visit, n);
-    return 1;
+
+    maxDist = bfs(list, dist, visit, n);
+    
+    for(int i=1; i<dist.length; i++){
+      if(dist[i] == maxDist)
+        cnt++;
+    }
+    return cnt;
   }
 
-  public static void bfs(ArrayList<Integer>[] list, int[] dist, boolean[] visit, int n){
+  public static int bfs(ArrayList<Integer>[] list, int[] dist, boolean[] visit, int n){
     Queue<int[]> q = new LinkedList<>();
+    int maxDist = 0;
     
     q.offer(new int[]{1, 0});
     visit[1] = true;
+    dist[1] = 0;
 
     while(!q.isEmpty()){
       int[] poll = q.poll();
+      
+      for(int i=0; i<list[poll[0]].size(); i++){
+        int curNode = list[poll[0]].get(i);
 
-      if(poll[0] == n){
-        return;
+        if(!visit[curNode] ){
+          visit[curNode] = true;
+          dist[curNode] = dist[poll[0]]+1;
+          maxDist = Math.max(maxDist, dist[curNode]);
+          q.offer(new int[]{curNode, dist[curNode]});
+        }
       }
-
-      while(!visit[poll[0]]){}
     }
-
+    
+    return maxDist;
   }
   
 }
