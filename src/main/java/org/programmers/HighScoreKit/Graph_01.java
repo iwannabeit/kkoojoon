425b19d25896ec9ba3146c1dc1e2b8d83649b270
iwannabeit <skzx737@naver.com>
2026-06-23 10:57:28 +0900

    .

--- src/main/java/org/programmers/HighScoreKit/Graph_01.java
+++ src/main/java/org/programmers/HighScoreKit/Graph_01.java
@@ -7,9 +7,40 @@ public class Graph_01 {
     
   }
 
-  public static void bfs(int n, int[][] edge){
-    Queue q = new LinkedList<>();
+  static int solution(int n, int[][] edges){
+    int[] dist = new int[n+1];
+    boolean[] visit = new boolean[n+1];
+    ArrayList<Integer>[] list = new ArrayList[n+1];
+
+    for(int i=0; i<=n; i++){
+      list[i] = new ArrayList<>();
+    }
+
+    for(int[] edge: edges){
+      list[edge[0]].add(edge[1]);
+      list[edge[1]].add(edge[0]);
+    }
+
+    bfs(list, dist, visit, n);
+    return 1;
+  }
+
+  public static void bfs(ArrayList<Integer>[] list, int[] dist, boolean[] visit, int n){
+    Queue<int[]> q = new LinkedList<>();
     
+    q.offer(new int[]{1, 0});
+    visit[1] = true;
+
+    while(!q.isEmpty()){
+      int[] poll = q.poll();
+
+      if(poll[0] == n){
+        return;
+      }
+
+      while(!visit[poll[0]]){}
+    }
+
   }
   
 }
